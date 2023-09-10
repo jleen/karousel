2869c21cb680b844ae1220d0714666b39ceb336c
jleen@VicViper <jleen@VicViper>
2023-09-10 18:20:06 -0400

    Recurse!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,11 @@
 import java.nio.file.Path
 import kotlin.io.path.Path
+import kotlin.io.path.extension
 import kotlin.io.path.isDirectory
 import kotlin.io.path.listDirectoryEntries
 
 fun main(args: Array<String>) {
-    val files = Path(args[0]).listDirectoryEntries()
-    files.forEach {
-        when {
-            it.isDirectory() -> traverse(it)
-            it.endsWith(".jpeg") -> render(it)
-        }
-    }
+    traverse(Path(args[0]))
 }
 
 fun render(photo: Path) {
@@ -19,4 +14,11 @@ fun render(photo: Path) {
 
 fun traverse(dir: Path) {
     println("Traversing directory $dir")
+    val files = dir.listDirectoryEntries()
+    files.forEach {
+        when {
+            it.isDirectory() -> traverse(it)
+            it.extension == "jpeg" -> render(it)
+        }
+    }
 }

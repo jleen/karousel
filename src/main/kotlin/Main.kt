9fa20591b1e0966be944ff78447feb598a10d116
jleen@VicViper <jleen@VicViper>
2023-09-10 21:23:50 -0400

    Top-down scaffolding

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,21 +4,58 @@ import kotlin.io.path.extension
 import kotlin.io.path.isDirectory
 import kotlin.io.path.listDirectoryEntries
 
+val VIEW_SIZE: Int = 0
+val PREVIEW_SIZE: Int = 0
+
 fun main(args: Array<String>) {
-    traverse(Path(args[0]))
+    traverseDirectory(Path(args[0]))
 }
 
-fun render(photo: Path) {
-    println("Handling photo $photo")
-}
-
-fun traverse(dir: Path) {
+fun traverseDirectory(dir: Path) {
     println("Traversing directory $dir")
+    renderDirectoryPage(dir)
+
     val files = dir.listDirectoryEntries()
     files.forEach {
         when {
-            it.isDirectory() -> traverse(it)
-            it.extension == "jpeg" -> render(it)
+            it.isDirectory() -> traverseDirectory(it)
+            it.extension == "jpeg" -> traversePhoto(it)
         }
     }
+}
+
+fun traversePhoto(photo: Path) {
+    println("Handling photo $photo")
+    renderPhotoPage(photo)
+    renderPreview(photo)
+    renderView(photo)
+    renderFull(photo)
+}
+
+fun renderFull(photo: Path) {
+    copyPhoto(photo)
+}
+
+fun renderPreview(photo: Path) {
+    resizePhoto(photo, PREVIEW_SIZE)
+}
+
+fun renderView(photo: Path) {
+    resizePhoto(photo, VIEW_SIZE)
+}
+
+fun renderPhotoPage(photo: Path) {
+    TODO("Not yet implemented")
+}
+
+fun renderDirectoryPage(dir: Path) {
+    TODO("Not yet implemented")
+}
+
+fun copyPhoto(photo: Path) {
+    TODO("Not yet implemented")
+}
+
+fun resizePhoto(photo: Path, previewSize: Int) {
+    TODO("Not yet implemented")
 }
